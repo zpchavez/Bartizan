3a75233ef7d200f229398db20c04afb2936f0ff7
Johnny A <johnny@synapsestudios.com>
2018-06-28 13:06:54 -0700

    Increase chance of 8-bit music to 50%

---
+++ Mod/Classes/Patched/MyMainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Monocle;
+using Patcher;
+using TowerFall;
+
+namespace Mod
+{
+	[Patch]
+	public class MyMainMenu : MainMenu
+    {
+		public MyMainMenu(MenuState state) : base(state)
+        {
+        }
+
+		public new static void PlayMenuMusic(bool fromArchives = false, bool forceNormal = false)
+        {
+			forceNormal = false;
+            if (!(Music.CurrentSong == "AltTitle") && !(Music.CurrentSong == "ChipTitle") && !(Music.CurrentSong == "Title"))
+            {
+                if (forceNormal)
+					{
+                    Music.Play("AltTitle");
+                    MainMenu.wasChipTheme = false;
+                }
+                else if (fromArchives)
+                {
+                    if (MainMenu.wasChipTheme)
+                    {
+                        Music.Play("ChipTitle");
+                    }
+                    else
+                    {
+                        Music.Play("AltTitle");
+                    }
+                }
+                else if (Calc.Chance(Calc.Random, 0.5f))
+                {
+                    Music.Play("ChipTitle");
+                    MainMenu.wasChipTheme = true;
+                }
+                else
+                {
+                    Music.Play("AltTitle");
+                    MainMenu.wasChipTheme = false;
+                }
+            }
+        }
+    }
+}
