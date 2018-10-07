74c6d7bede539dcad8bdf6c9aa17d0a32729f2ef
Zachary Chavez <zpchavez@gmail.com>
2018-10-06 18:17:41 -0700

    Untabify file

--- Mod/Classes/Patched/MyLevel.cs
+++ Mod/Classes/Patched/MyLevel.cs
@@ -10,23 +10,23 @@ using System.Xml;
 
 namespace Mod
 {
-	[Patch]
-	public class MyLevel : Level
+  [Patch]
+  public class MyLevel : Level
   {
-		public MyLevel (Session session, XmlElement xml) : base(session, xml)
-		{
-		}
+    public MyLevel (Session session, XmlElement xml) : base(session, xml)
+    {
+    }
 
-		public override void Update ()
-		{
-			base.Update();
-			List<Entity> teamRevivers = base[GameTags.Dummy]; // Using Dummy tag for MyTeamReviver
-			for (int i = 0; i < teamRevivers.Count; i++) {
-				MyTeamReviver teamReviver = (MyTeamReviver)(teamRevivers[i]);
-				if (teamReviver.Active) {
-					teamReviver.ReviveUpdate ();
-				}
-			}
-		}
+    public override void Update ()
+    {
+      base.Update();
+      List<Entity> teamRevivers = base[GameTags.Dummy]; // Using Dummy tag for MyTeamReviver
+      for (int i = 0; i < teamRevivers.Count; i++) {
+        MyTeamReviver teamReviver = (MyTeamReviver)(teamRevivers[i]);
+        if (teamReviver.Active) {
+          teamReviver.ReviveUpdate ();
+        }
+      }
+    }
   }
 }
