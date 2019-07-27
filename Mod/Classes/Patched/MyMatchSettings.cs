9a94edf68db3252fdb4c417a73c30f4143feea4b
Zachary Chavez <zpchavez@gmail.com>
2019-07-27 11:59:47 -0700

    Fix wrong number of method args for 4-player

--- Mod/Classes/Patched/MyMatchSettings.cs
+++ Mod/Classes/Patched/MyMatchSettings.cs
@@ -20,7 +20,7 @@ namespace Mod
 						#if (EIGHT_PLAYER)
 							int goals = this.PlayerGoals(5, 8, 10, 10, 10, 10, 10);
 						#else
-							int goals = this.PlayerGoals(5, 8, 10, 12);
+							int goals = this.PlayerGoals(5, 8, 10);
 						#endif
 						return (int)Math.Ceiling(((float)goals * MatchSettings.GoalMultiplier[(int)this.MatchLength]));
 					default:
