01661abb73e86ec48f539ea4a015013330a6732e
Zachary Chavez <zpchavez@gmail.com>
2020-12-19 21:04:17 -0700

    Fix ghosts able to pick up orbs with ghost items variant off

--- Mod/Classes/Patched/MyOrbPickup.cs
+++ Mod/Classes/Patched/MyOrbPickup.cs
@@ -16,33 +16,36 @@ namespace Mod
 
         public override void OnPlayerGhostCollide(PlayerGhost ghost)
         {
-            base.Level.Layers[ghost.LayerIndex].Add(new LightFade().Init(this, null));
-            base.DoCollectStats(ghost.PlayerIndex);
-            switch (this.orbType)
+            if (((MyMatchVariants)Level.Session.MatchSettings.Variants).GhostItems)
             {
-                case OrbTypes.Dark:
-                    Sounds.pu_darkOrbCollect.Play(base.X, 1f);
-                    base.Level.OrbLogic.DoDarkOrb();
-                    base.Level.Particles.Emit(Particles.DarkOrbCollect, 12, base.Position, Vector2.One * 4f);
-                    break;
-                case OrbTypes.Time:
-                    Sounds.pu_darkOrbCollect.Play(base.X, 1f);
-                    base.Level.OrbLogic.DoTimeOrb(false);
-                    base.Level.Particles.Emit(Particles.TimeOrbCollect, 12, base.Position, Vector2.One * 4f);
-                    break;
-                case OrbTypes.Lava:
-                    Sounds.pu_lava.Play(base.X, 1f);
-                    base.Level.OrbLogic.DoLavaOrb(ghost.PlayerIndex);
-                    base.Level.Particles.Emit(Particles.LavaOrbCollect, 12, base.Position, Vector2.One * 4f);
-                    break;
-                case OrbTypes.Space:
-                    Sounds.pu_spaceOrb.Stop(true);
-                    Sounds.pu_spaceOrb.Play(210f, 1f);
-                    base.Level.OrbLogic.DoSpaceOrb();
-                    base.Level.Particles.Emit(Particles.SpaceOrbCollect, 12, base.Position, Vector2.One * 4f);
-                    break;
+                base.Level.Layers[ghost.LayerIndex].Add(new LightFade().Init(this, null));
+                base.DoCollectStats(ghost.PlayerIndex);
+                switch (this.orbType)
+                {
+                    case OrbTypes.Dark:
+                        Sounds.pu_darkOrbCollect.Play(base.X, 1f);
+                        base.Level.OrbLogic.DoDarkOrb();
+                        base.Level.Particles.Emit(Particles.DarkOrbCollect, 12, base.Position, Vector2.One * 4f);
+                        break;
+                    case OrbTypes.Time:
+                        Sounds.pu_darkOrbCollect.Play(base.X, 1f);
+                        base.Level.OrbLogic.DoTimeOrb(false);
+                        base.Level.Particles.Emit(Particles.TimeOrbCollect, 12, base.Position, Vector2.One * 4f);
+                        break;
+                    case OrbTypes.Lava:
+                        Sounds.pu_lava.Play(base.X, 1f);
+                        base.Level.OrbLogic.DoLavaOrb(ghost.PlayerIndex);
+                        base.Level.Particles.Emit(Particles.LavaOrbCollect, 12, base.Position, Vector2.One * 4f);
+                        break;
+                    case OrbTypes.Space:
+                        Sounds.pu_spaceOrb.Stop(true);
+                        Sounds.pu_spaceOrb.Play(210f, 1f);
+                        base.Level.OrbLogic.DoSpaceOrb();
+                        base.Level.Particles.Emit(Particles.SpaceOrbCollect, 12, base.Position, Vector2.One * 4f);
+                        break;
+                }
+                base.RemoveSelf();
             }
-            base.RemoveSelf();
         }
     }
 }
